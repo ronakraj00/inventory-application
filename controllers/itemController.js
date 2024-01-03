9495ba651b349c4172b6e5e124a24cb3c1c5c797
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-01-03 11:41:28 +0530

    fix update item feature

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -85,15 +85,57 @@ exports.get_item_update = [
         const allCollections = await collection.find().sort({ name: 1 });
         const item = await Item.findById(id);
         if (item) {
-            const clonedItem = item;
-            await Item.findByIdAndDelete(item.id);
             res.render("item_form", {
                 title: "Create Item",
                 allCollections: allCollections,
-                item: clonedItem,
+                item: item,
             });
         } else {
             res.render("/");
+        }
+    }),
+];
+
+exports.post_item_update = [
+    (req, res, next) => {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect("/login");
+    },
+    upload.single("img"),
+    body("name")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Name should be atleast one word"),
+    body("description").optional({ values: "falsy" }),
+    body("category").optional({ values: "falsy" }),
+    body("collection").optional({ values: "falsy" }),
+    body("price").optional({ values: "falsy" }),
+    body("stock").optional({ values: "falsy" }),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        const item = new Item({
+            _id:req.params.id,
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            collection: req.body.collection,
+            price: req.body.price,
+            stock: req.body.stock,
+            img: req.file ? req.file.filename : req.body.imgUrl,
+        });
+        if (!errors.isEmpty()) {
+            const allCollections = await collection.find().sort({ name: 1 });
+            res.render("item_form", {
+                title: "Create new Item",
+                allCollections: allCollections,
+                errors: errors.array(),
+            });
+        } else {
+            await Item.findByIdAndUpdate(req.params.id, item, {});
+            res.redirect(`/collection/${req.body.collection}`);
         }
     }),
 ];
--- routes/collection.js
+++ routes/collection.js
@@ -9,7 +9,7 @@ router.get("/item/create", itemController.get_item_create);
 router.post("/item/create", itemController.post_item_create);
 router.get("/item/delete/:id",itemController.post_item_delete);
 router.get("/item/update/:id",itemController.get_item_update);
-// router.post("/item/update/:id",itemController.post_item_update);
+router.post("/item/update/:id",itemController.post_item_update);
 router.get("/delete/:id",collectionController.post_collection_delete);
 router.get("/:id", collectionController.collection_items_list);
 
